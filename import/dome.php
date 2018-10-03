651c3114015d0860ae7be2b979a23ea10fd30521
xue-y <1922527784@qq.com>
2018-10-03 16:31:48 +0800

    添加测试版本说明

--- import/dome.php
+++ import/dome.php
@@ -7,4 +7,4 @@ namespace import;
 require "../vendor/autoload.php";
 //$back_name,$host,$db,$dbuser='',$dbpw='',$charset='utf8',$prot=3306,$is_del=true
 // 提示文字 在 Import.php 290 line
-new ImportData("test",'127.0.0.1','t','root','admin');
+new ImportData("t",'127.0.0.1','test','root','admin');
